af149a03d347452a5c5851991216dea321517f1d
Suryadeep Pal <suryadeep10@gmail.com>
2015-12-19 21:31:08 +0530

    websockets configuration changed

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+}); 
+
 /* Configuration */
 
 require('./config/configuration.js')(app, mongoose);
@@ -21,5 +26,5 @@ require('./lib/socketEvents/socketEvents.js')(io);
 // Messages = require('./models/Messages.js');
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on :' + process.env.PORT);
 });
