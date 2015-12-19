ce528d1d9c05ac6ec75c9a4deb2e00c4a95468b5
Suryadeep Pal <suryadeep10@gmail.com>
2015-12-19 08:47:57 +0530

    index.js updated with http module for integration with socket.io

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 
 /* Configuration */
 
 require('./config/configuration.js')(app, mongoose);
+
+/* Routes */
+
+require('./routes/chatPage.js')(app);
+
+/* Socket events */
+
+require('./lib/socketEvents/socketEvents.js')(io);
+
+/* Models */
+
+Users = require('./models/Users.js');
+Messages = require('./models/Messages.js');
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
