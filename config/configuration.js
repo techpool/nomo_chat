3553558a854b51f4d153c6a89138cd078c236f05
Suryadeep Pal <suryadeep10@gmail.com>
2015-12-20 23:02:06 +0530

    config file changed with mongolab connection

--- config/configuration.js
+++ config/configuration.js
@@ -1,15 +1,16 @@
 module.exports = function(app, mongoose){
 
-    app.set('db', 'mongodb://localhost:27017/nomochat');
+    //app.set('db', 'mongodb://localhost:27017/nomochat');
+    app.set('db', 'mongodb://Suryadeep:Surya-23121994@ds033285.mongolab.com:33285/mongocloud');
+
     app.set('views', './views');
     app.set('view engine', 'jade');
 
-
-    // mongoose.connect(app.get('db'));
-    // var db = mongoose.connection;
-    // db.on('error', console.error.bind(console, 'connection error:'));
-    // db.once('open', function callback(){
-    //     console.log('db connected');
-    // });
+    mongoose.connect(app.get('db'));
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function callback(){
+        console.log('db connected');
+    });
 
 };
