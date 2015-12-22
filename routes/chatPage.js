2a0cfe8533b24036abd8ac870d33ec78d2b78935
Suryadeep Pal <suryadeep10@gmail.com>
2015-12-22 23:45:33 +0530

    added route for registration

--- routes/chatPage.js
+++ routes/chatPage.js
@@ -2,8 +2,12 @@ var path = require('path');
 var appDir = path.dirname(require.main.filename);
 
 module.exports = function (app) {
-    
+
     app.get('/', function (req, res) {
         res.render('index');
+    });
+
+    app.get('/register', function (req, res) {
+        res.render('register');
     });
 }
