e7666b7046cd0092ff9bbca5088190642a7e8254
Suryadeep Pal <suryadeep10@gmail.com>
2015-12-24 20:59:39 +0530

    body-parser added to parse the body

--- config/configuration.js
+++ config/configuration.js
@@ -1,10 +1,17 @@
+var bodyParser = require('body-parser');
+
 module.exports = function(app, mongoose){
 
-    //app.set('db', 'mongodb://localhost:27017/nomochat');
-    app.set('db', 'mongodb://Suryadeep:Surya-23121994@ds033285.mongolab.com:33285/mongocloud');
+    app.set('db', 'mongodb://localhost:27017/nomochat');
+    //app.set('db', 'mongodb://Suryadeep:Surya-23121994@ds033285.mongolab.com:33285/mongocloud');
 
+    //Setting up view engine as Jade
     app.set('views', './views');
     app.set('view engine', 'jade');
+
+    //Setting up the body-parser
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
     mongoose.connect(app.get('db'));
     var db = mongoose.connection;
